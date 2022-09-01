eed47e0493ed69394bceb2e678217ec1a8c79e91
Brittany Yarbrough <brittanymyar@gmail.com>
2022-08-31 20:33:04 -0400

    add form

--- server.js
+++ server.js
@@ -6,14 +6,29 @@ const budget = require("./models/budget.js")
 const app = express();
 const port = 3000;
 
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));
+
+
 
 // DEFINE OUR ROUTES
-app.get("/budget/" , (request, respond) => {
+app.get("/budgets/" , (request, respond) => {
     respond.render("index.ejs", {
         allBudget: budget
     })
 
 });
+app.get("/budgets/new", (request, respond) => {
+    respond.render("new.ejs")
+});
+
+app.get("/budgets/:index", (req, res)=>{
+    res.render("show.ejs", {
+        budgets: budget[req.params.index]
+    })
+});
+
+
 
 
 
